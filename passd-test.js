b844d35b00350266fe2785136abe047588525110
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 15:30:24 +0800

    Updated test.

--- passd-test.js
+++ passd-test.js
@@ -4,16 +4,16 @@ const passd = require( "./passd.js" );
 
 let CALLED_ONCE = Symbol( "called-once" );
 let callback = function callback( ){
-	assert.deepEqual( Array.from( arguments ), [ "hello", "world", "yeah" ], "should be deeply equal" );
+	assert.deepEqual( Array.from( arguments ), [ "hello", "world", "yeah" ], "should be equal to [ 'hello', 'world', 'yeah' ]" );
 };
 callback[ CALLED_ONCE ] = CALLED_ONCE;
 
 passd( callback, function test( ){
-	assert.deepEqual( Array.from( arguments ), [ "hello", "world", "yeah" ], "should be deeply equal" );
+	assert.deepEqual( Array.from( arguments ), [ "hello", "world", "yeah" ], "should be equal to [ 'hello', 'world', 'yeah' ]" );
 
 	console.log( "ok" );
 } );
 
-assert.equal( typeof callback.passed == "function", true, "should have passed function" );
+assert.equal( typeof callback.passed == "function", true, "should passed function" );
 
 callback.passed( "hello", "world", "yeah" );
